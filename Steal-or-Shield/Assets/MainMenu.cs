dcc638fb1e8292c3d6a26c91b3ac0ea26360a98f
julieose <julianureevs@gmail.com>
2024-11-21 22:37:24 +0300

    доработаны кнопки play и exit

--- Steal-or-Shield/Assets/MainMenu.cs
+++ Steal-or-Shield/Assets/MainMenu.cs
@@ -1,16 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public void PlayGame()
     {
-        
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Exitgame()
     {
-        
+        Debug.Log("The game is closed");
+        Application.Quit();
     }
 }
