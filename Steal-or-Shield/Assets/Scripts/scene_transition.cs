fcb34f4015614d739f5a6ec4c3f4a9217a511870
Nastya-22-06 <nastya_22_06@mail.ru>
2025-03-28 19:06:26 +0300

    Добавлены сцены и рандомный выбор сцен

--- Steal-or-Shield/Assets/Scripts/scene_transition.cs
+++ Steal-or-Shield/Assets/Scripts/scene_transition.cs
@@ -1,3 +1,4 @@
+using UnityEditor.SearchService;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -7,5 +8,13 @@ public class scene_transition : MonoBehaviour
     {
         //SceneManager.LoadScene("bedroom");
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    public void LoadRandomScene()
+    {
+        //string[] scenes = new string[3] { "bedroom", "bedroom1", "bedroom2" };
+        //string randomSceneName = scenes[Random.Range(0, scenes.Length)];
+        //SceneManager.LoadScene(randomSceneName);
+        int scena = Random.Range(1, 4);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + scena);
     }
 }
