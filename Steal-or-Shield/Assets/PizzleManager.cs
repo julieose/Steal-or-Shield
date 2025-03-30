dab6c25f201cabaa2581d59e99bad76d58653ada
Nastya-22-06 <nastya_22_06@mail.ru>
2025-03-30 19:47:12 +0300

    Добавлена музыка, победная надпись

--- Steal-or-Shield/Assets/PizzleManager.cs
+++ Steal-or-Shield/Assets/PizzleManager.cs
@@ -1,9 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PuzzleManager : MonoBehaviour
 {
+    public GameObject myGameObject;//объект после победной игры
+    public AudioClip bcgMusic;// музыка победы
     private GameObject[] puzzle;
     public static bool isCorrect;
 
@@ -11,6 +14,7 @@ public class PuzzleManager : MonoBehaviour
     {
         puzzle = GameObject.FindGameObjectsWithTag("Puzzle");
         isCorrect = false;
+        myGameObject.SetActive(false);
     }
 
     private void Update()
@@ -18,7 +22,7 @@ public class PuzzleManager : MonoBehaviour
         bool allTrue = true;
         foreach (var item in puzzle)
         {
-            if(item.transform.rotation.z < -0.01 || item.transform.rotation.z > 0.01)
+            if (item.transform.rotation.z < -0.01 || item.transform.rotation.z > 0.01)
             {
                 allTrue = false; break;
             }
@@ -26,8 +30,91 @@ public class PuzzleManager : MonoBehaviour
         if (allTrue)
         {
             isCorrect = true;
+            StartCoroutine(WinSequence());
             Debug.Log("YOU WIN");
+
         }
         PuzzleRotate.isMouse = false;
+    }
+
+    //IEnumerator Pause(float time)
+    //{ 
+    //    myGameObject.SetActive(true);
+    //    //AudioSource audioSource = gameObject.GetComponent<AudioSource>();//определяю компонент с музыкой
+    //    //if (audioSource.isPlaying)
+    //    //{
+    //    //    audioSource.Pause();//ставлю на паузу фоновую музыку
+    //    //}
+    //    //AudioSource.PlayClipAtPoint(bcgMusic, transform.position);
+    //    Cursor.visible = false; //курсор не видно
+    //    // Активируем объект с победой
+
+
+    //    // Ждем 7 секунд
+    //    yield return new WaitForSecondsRealtime(time);
+
+    //    // Деактивируем объект с победой
+    //    myGameObject.SetActive(false);
+
+
+    //    Cursor.visible = true;//включаю курсор
+    //    //audioSource.Play();//включаю фоновую музыку
+    //    SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    //}
+    //IEnumerator Pause1(float time)
+    //{
+
+    //    // Ждем 7 секунд
+    //    yield return new WaitForSecondsRealtime(time);
+
+    //    StartCoroutine(Pause(5));
+
+    //}
+
+    //IEnumerator WinSequence()
+    //{
+
+    //    // Активируем объект с победой
+    //    myGameObject.SetActive(true);
+    //    AudioSource audioSource = gameObject.GetComponent<AudioSource>();//определяю компонент с музыкой
+    //    if (audioSource.isPlaying)
+    //    {
+    //        audioSource.Pause();//ставлю на паузу фоновую музыку
+    //    }
+    //    AudioSource.PlayClipAtPoint(bcgMusic, transform.position);
+
+    //    // Ждём 5 секунды
+    //    yield return new WaitForSecondsRealtime(5);
+
+    //    // Деактивируем объект с победой
+    //    myGameObject.SetActive(false);
+
+    //    // Переходим на следующую сцену
+    //    SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    //}
+
+    IEnumerator WinSequence()
+    {
+        // Активируем объект с победой
+        myGameObject.SetActive(true);
+
+        // Ставим на паузу фоновую музыку
+        AudioSource audioSource = gameObject.GetComponent<AudioSource>();
+        if (audioSource != null && audioSource.isPlaying)
+        {
+            audioSource.Pause();
+        }
+
+        // Проигрываем победную музыку
+        AudioSource.PlayClipAtPoint(bcgMusic, transform.position);
+
+        // Ждём 5 секунд
+        yield return new WaitForSecondsRealtime(5);
+
+        // Деактивируем объект с победой
+        myGameObject.SetActive(false);
+
+        // Переходим на следующую сцену
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 }
